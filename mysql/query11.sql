ae3eaf88d8ca00298d6934714a22b456ffa31310
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2025-05-09 16:54:38 -0300

    Create query11.sql

---
+++ mysql/query11.sql
@@ -0,0 +1,10 @@
+select 
+row_number() over (order by count(enc.idcliente) desc) as ranking,
+cli.nome as cliente,
+count(enc.idcliente) as qtd_pedido
+from cliente cli
+join encomenda enc
+on cli.id = enc.idcliente
+group by cli.nome
+order by count(enc.idcliente) desc
+limit 10;
